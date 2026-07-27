d39948d63821ae67bbae02d0673e9e559a32b169
kimiasaadat54-a11y <kimiasaadat54@gmail.com>
2026-07-27 12:43:06 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1873,6 +1873,7 @@ export default function App() {
                         <option value={20}>20</option>
                         <option value={50}>50</option>
                         <option value={100}>100</option>
+                        <option value={200}>100</option>
                       </select>
                     </div>
 
